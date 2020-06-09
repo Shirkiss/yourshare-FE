2befa511227f81bac08c828d635a68222addb225
shir.cohen <shir.cohen@myheritage.com>
2020-06-09 20:04:25 +0300

    #description [BE] Getting name from logged in user when posting a comments #is_emergency #reviewer #tester #unit_tests #jira #flag #sensor #remark

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,31 +1,29 @@
 import React, { useState } from 'react';
+import { Auth} from 'aws-amplify';
+
 
 const AddCommentForm = ( {articleName, setArticleInfo} ) => {
 
-    const [username, setUserName] = useState('');
     const [commentText, setCommentText] = useState('');
 
     const addComment = async () => {
+        const tokens = await Auth.currentSession();
+        const userName = tokens.getIdToken().payload['cognito:username'];
         const result = await fetch(`/api/articles/${articleName}/add-comment`, {
             method: 'post',
-            body: JSON.stringify({username, text: commentText}),
+            body: JSON.stringify({username: userName, text: commentText}),
             headers: {
                 'Content-Type': 'application/json'
             }
         });
         const body = await result.json();
         setArticleInfo(body);
-        setUserName('');
         setCommentText('');
     };
 
     return (
         <div id="add-comment-form">
             <h3>Add a Comment</h3>
-            <label>
-                Name:
-                <input type="text" value={username} onChange={(event) => setUserName(event.target.value)}/>
-            </label>
             <label>
                 Comment:
                 <textarea rows="4" cols="50" value={commentText} onChange={(event) => setCommentText(event.target.value)}/>
